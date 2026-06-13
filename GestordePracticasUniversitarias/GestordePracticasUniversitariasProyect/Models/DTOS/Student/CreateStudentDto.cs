d9aaea18776b3338dfa8e54e6015b1aed3d4c001
Toribio360 <20242551@itla.edu.do>
2026-06-13 00:08:10 -0400

    Adding relation between Student and Company

---
+++ GestordePracticasUniversitarias/GestordePracticasUniversitariasProyect/Models/DTOS/Student/CreateStudentDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestordePracticasUniversitariasProyect.Models.DTOS.Student
+{
+    public class CreateStudentDto
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+    
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+      
+        public string Lastname { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string? Address { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Debe indicar una empresa válida")]
+        public int CompanyId { get; set; }
+    }
+}
--- GestordePracticasUniversitarias/GestordePracticasUniversitariasProyect/Models/Entites/Company.cs
+++ GestordePracticasUniversitarias/GestordePracticasUniversitariasProyect/Models/Entites/Company.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace GestordePracticasUniversitariasProyect.Models.Entites
 {
@@ -6,12 +7,14 @@ namespace GestordePracticasUniversitariasProyect.Models.Entites
     {
 
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
 
         public string Address { get; set; } = string.Empty;
         public string Email { get; set; } =string.Empty;
-  
-        public int Studentid { get; set; }
+
+        public List<Student> Students { get; set; } = new();
+    
 
         public bool IsActive { get; set; } = true;
 
